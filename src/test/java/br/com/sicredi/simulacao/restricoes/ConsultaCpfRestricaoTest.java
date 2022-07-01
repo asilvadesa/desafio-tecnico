e407c71e283a39ea3c9662a9b96e0b77a04e7e64
Anderson Silva de Sa <asilvadesa@gmail.com>
2022-07-01 00:12:58 -0300

    Refatorando validacao do teste

--- src/test/java/br/com/sicredi/simulacao/restricoes/ConsultaCpfRestricaoTest.java
+++ src/test/java/br/com/sicredi/simulacao/restricoes/ConsultaCpfRestricaoTest.java
@@ -1,10 +1,15 @@
 package br.com.sicredi.simulacao.restricoes;
 
-import br.com.sicredi.simulacao.service.SimulacaoCreditoService;
 import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.text.MessageFormat;
+
+import static br.com.sicredi.simulacao.service.SimulacaoCreditoService.consultaCpfSeTemRestricao;
+import static org.hamcrest.Matchers.is;
 
 @DisplayName("Consulta Status do CPF")
 public class ConsultaCpfRestricaoTest {
@@ -13,7 +18,9 @@ public class ConsultaCpfRestricaoTest {
     @CsvFileSource(resources = "/cpfComRestricao.csv")
     @DisplayName("Consulta deve retornar CPF com restrição")
     void validaConsultaCpfComRestricaoTest(String cpf){
-        Response response = SimulacaoCreditoService.consultaCpfSeTemRestricao(cpf);
-        System.out.println(response.then().log().all());
+        Response response = consultaCpfSeTemRestricao(cpf);
+        response.then()
+        .statusCode(HttpStatus.SC_OK)
+        .body("mensagem", is(MessageFormat.format("O CPF {0} tem problema", cpf)));
     }
 }
