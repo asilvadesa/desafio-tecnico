eb734293c088bf147fd753b1f23dfc46c46da77c
Anderson Silva de Sa <asilvadesa@gmail.com>
2022-07-04 12:55:51 -0300

    Adcioando specs de validacao

--- src/main/java/br/com/sicredi/simulacao/specs/request/SimulacaoCreditoRequestSpec.java
+++ src/main/java/br/com/sicredi/simulacao/specs/request/SimulacaoCreditoRequestSpec.java
@@ -1,13 +1,15 @@
-package br.com.sicredi.simulacao.specs;
+package br.com.sicredi.simulacao.specs.request;
 
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
-public class SimulacaoCreditoSpec {
+public class SimulacaoCreditoRequestSpec {
 
     public static RequestSpecification simulacaoCreditoSpecs(){
         return new RequestSpecBuilder()
                 .setBaseUri("http://localhost")
+                .setContentType(ContentType.JSON)
                 .setPort(8080)
                 .build();
     }
---
+++ src/main/java/br/com/sicredi/simulacao/specs/response/SimulacaoCreditoResponseSpec.java
@@ -0,0 +1,47 @@
+package br.com.sicredi.simulacao.specs.response;
+
+import br.com.sicredi.simulacao.domain.Simulacao;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.ResponseSpecification;
+import org.apache.http.HttpStatus;
+
+import static org.hamcrest.Matchers.*;
+
+public class SimulacaoCreditoResponseSpec {
+    public static ResponseSpecification validaConsultaTodasSimulacoesSpec(){
+        return new ResponseSpecBuilder()
+                .expectStatusCode(HttpStatus.SC_OK)
+                .expectBody("id.size", is(notNullValue()))
+                .expectBody("id.size", is(greaterThan(1)))
+                .build();
+    }
+
+    public static ResponseSpecification validaCadastroNovaSimulacaoSpec(Simulacao simulacao){
+        return new ResponseSpecBuilder()
+                .expectStatusCode(HttpStatus.SC_CREATED)
+                .expectBody("nome", is(simulacao.getNome()))
+                .expectBody("cpf", is(simulacao.getCpf()))
+                .expectBody("email",is(simulacao.getEmail()))
+                .expectBody("valor", is(simulacao.getValor()))
+                .expectBody("parcelas",is(simulacao.getParcelas()))
+                .expectBody("seguro", is(simulacao.getSeguro()))
+                .build();
+    }
+
+    public static ResponseSpecification validaCadastroNovaSimulacaoSemInformacaoDeParcelaSpec(){
+        return new ResponseSpecBuilder()
+                .expectStatusCode(HttpStatus.SC_BAD_REQUEST)
+                .expectBody("erros.parcelas", is(notNullValue()))
+                .expectBody("erros.parcelas", is("Parcelas não pode ser vazio"))
+                .build();
+    }
+
+    public static ResponseSpecification validaCadastroNovaSimulacaoSemInformacaoDeParcelaEValorSpec(){
+        return new ResponseSpecBuilder()
+                .expectStatusCode(HttpStatus.SC_BAD_REQUEST)
+                .expectBody("erros.parcelas", is(notNullValue()))
+                .expectBody("erros.parcelas", is("Parcelas não pode ser vazio"))
+                .expectBody("erros.valor", is("Valor não pode ser vazio"))
+                .build();
+    }
+}
